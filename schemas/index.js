2e728db74a9c50b038346655fb6a376f58e2d3e3
RasmusKodehode <rasmus.kodehode@gmail.com>
2024-06-03 10:12:42 +0200

    deleted unused schemas and added space for new imports

--- schemas/index.js
+++ schemas/index.js
@@ -1,14 +1,21 @@
-import carouselleMeeting from "./carouselleMeeting"
-import carouselleOffice from "./carouselleOffice"
-import carouselleMedia from "./carouselleMedia"
-import carouselleKitchen from "./carouselleKitchen"
-import modalData from "./modalData"
-import memeData from "./memeData"
-import cafeMenu from "./cafeMenu"
+//import schema1 //kommenter ut denne linjen
+//import schema2 //kommenter ut denne linjen
+//import schema3 //kommenter ut denne linjen
+//import schema4 //kommenter ut denne linjen
+//import schema5 //kommenter ut denne linjen
+//import schema6 //kommenter ut denne linjen
+//import schema7 //kommenter ut denne linjen
 import lunch from "./lunch"
 import schedule from './schedule'
 
 export const schemaTypes = [
-    lunch,
-    schedule,
+  lunch,
+  schedule,
+  //schema1,
+  //schema2,
+  //schema3,
+  //schema4,
+  //schema5,
+  //schema6,
+  //schema7,
 ]
