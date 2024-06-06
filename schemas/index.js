5160575766b43a43dee7dc89c75bb61527d6b0f6
steiningekodehode <steininge.kodehode@gmail.com>
2024-06-06 10:26:46 +0200

    cleanup

--- schemas/index.js
+++ schemas/index.js
@@ -1,13 +1,8 @@
 import ahead from "./ahead"
 import kafe from "./kafe"
 import hiking from "./hiking"
-//import schema4 //kommenter ut denne linjen
-//import schema5 //kommenter ut denne linjen
-//import schema6 //kommenter ut denne linjen
-//import schema7 //kommenter ut denne linjen
 import lunch from "./lunch"
 import schedule from './schedule'
-
 import imagepages from './imagepages'
 
 export const schemaTypes = [
@@ -17,8 +12,4 @@ export const schemaTypes = [
   ahead,
   kafe,
   hiking,
-  //schema4,
-  //schema5,
-  //schema6,
-  //schema7,
-]
+  ]
