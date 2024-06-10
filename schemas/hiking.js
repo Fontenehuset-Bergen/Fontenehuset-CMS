a63dc11dfa4b1d1de4d4a1d7a1078cf536fc21b6
AndyKodehode <andykodehode@gmail.com>
2024-06-10 18:41:21 +0200

    hiking clothes, image and description

--- schemas/hiking.js
+++ schemas/hiking.js
@@ -8,84 +8,107 @@ export default {
             type: 'image',
             title: 'Destinasjonsbilde',
           },
-      {
-        name: 'placeName',
-        type: 'string',
-        title: 'Tursted',
-      },
-      {
-        name: 'date',
-        type: 'datetime',
-        title: 'Dato',
-      },
-      {
-        name: 'difficultyLevel',
-        type: 'string',
-        title: 'Vanskelighetsgrad',
-      },
-      {
-        name: 'timeUse',
-        type: 'string',
-        title: 'Tidsbruk',
-      },
-      {
-        name: 'height',
-        type: 'string',
-        title: 'Høyde',
-      },
-      {
-        name: 'distance',
-        type: 'string',
-        title: 'Distanse',
-      },
-      {
-        name: 'weather',
-        type: 'string',
-        title: 'Vær',
-      },
-      {
-        name: 'description',
-        type: 'string',
-        title: 'Turbeskrivelse',
-      },
-      {
-        name: 'access',
-        type: 'string',
-        title: 'Adkomst',
-      },
-      {
-        name: 'placesStop',
-        type: 'string',
-        title: 'Stoppesteder',
-      },
+          {
+            name: 'placeName',
+            type: 'string',
+            title: 'Tursted',
+          },
+          {
+            name: 'date',
+            type: 'datetime',
+            title: 'Dato',
+          },
+          {
+            name: 'difficultyLevel',
+            type: 'string',
+            title: 'Vanskelighetsgrad',
+          },
+          {
+            name: 'timeUse',
+            type: 'string',
+            title: 'Tidsbruk',
+          },
+          {
+            name: 'height',
+            type: 'string',
+            title: 'Høyde',
+          },
+          {
+            name: 'distance',
+            type: 'string',
+            title: 'Distanse',
+          },
+          {
+            name: 'weather',
+            type: 'string',
+            title: 'Vær',
+          },
+          {
+            name: 'description',
+            type: 'string',
+            title: 'Turbeskrivelse',
+          },
+          {
+            name: 'access',
+            type: 'string',
+            title: 'Adkomst',
+          },
+          {
+            name: 'placesStop',
+            type: 'string',
+            title: 'Stoppesteder',
+          },
   
-    {
-      name: 'clothingEquipment',
-      type: 'array',
-      title: 'Klær og utstyr',
-      of: [{ type: 'image', options: { hotspot: true } }],
-      description: 'Illustrasjoner av utstyr',
-  },
-      {
-        name: 'destinationMap',
-        type: 'image',
-        title: 'Destinasjonskart',
-      },
-      {
-        name: 'mainhikeImage',
-        type: 'image',
-        title: 'Hovedturbilde',
-      },
-      {
-        name: 'routeImage',
-        type: 'image',
-        title: 'Løype',
-      },
-      {
-        name: 'hikeImages',
-        type: 'image',
-        title: 'Turbilder',
-      },
+        {
+          name: 'clothingEquipment',
+          type: 'array',
+          title: 'Klær og utstyr',
+          of: [
+          
+            {
+              type: 'object',
+              fields: [
+            
+                {
+                  name: 'image',
+                  title: 'Image',
+                  type: 'image',
+                  options: {
+                    hotspot: true // Enables image cropping
+                  }
+                },
+                {
+                  name: 'description',
+                  title: 'Description',
+                  type: 'string'
+                }
+              ],
+            }
+          ]
+        },
+
+          
+
+                {
+                  name: 'destinationMap',
+                  type: 'image',
+                  title: 'Destinasjonskart',
+                },
+                {
+                  name: 'mainhikeImage',
+                  type: 'image',
+                  title: 'Hovedturbilde',
+                },
+                {
+                  name: 'routeImage',
+                  type: 'image',
+                  title: 'Løype',
+                },
+                {
+                  name: 'hikeImages',
+                  type: 'image',
+                  title: 'Turbilder',
+                },
     ],
   }
 
