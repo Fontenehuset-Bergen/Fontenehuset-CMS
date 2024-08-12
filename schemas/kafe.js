4ae92d082234696dbb1542d321cdd02f08e92d94
kristoffer <kunkristoffer@gmail.com>
2024-08-12 17:00:45 +0200

    Converting key names to english Adding required input on tittle field Converting Allergies field to array of strings

--- schemas/kafe.js
+++ schemas/kafe.js
@@ -1,33 +1,27 @@
 export default {
-    name: "kafe",
-    type: "document",
-    title: "Cafe",
-    fields: [
-
-        {
-    name: "title",
-    type: "string",
-    title: "Overskrift"
-        },
-        {
-            name: 'allergens',
-            type: 'string',
-            title: 'Allergener',
-          },
-          {
-            name: "beskrivelse",
-            type: "string",
-            title: "Beskrivelser",
-          },
-
-          {
-            name: 'image',
-            type: 'image',
-            title: 'Bilde',
-          },
-
-
-
-
-    ]
+  name: "coffee",
+  type: "document",
+  title: "kaffe",
+  fields: [
+    {
+      name: "name",
+      title: "Navn på drikke",
+      type: "string",
+      validation: rule => rule.required().min(3).max(20)
+    },{
+      name: 'allergies',
+      title: 'Allergener',
+      type: 'array',
+      of: [{type: 'string'}],
+      layout: 'tags'
+    },{
+      name: "desc",
+      title: "Beskrivelse",
+      type: "string",
+    },{
+      name: 'image',
+      title: 'Bilde',
+      type: 'image',
+    },
+  ]
 }
