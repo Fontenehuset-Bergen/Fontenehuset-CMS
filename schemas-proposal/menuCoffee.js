2857ba01f3a1562fe9018cc7a74b4c2a1eb3d5b3
kristoffer <kunkristoffer@gmail.com>
2024-08-19 16:39:39 +0200

    Adding new coffee document

---
+++ schemas-proposal/menuCoffee.js
@@ -0,0 +1,34 @@
+export default {
+  name: "menuCoffee",
+  type: "document",
+  title: "kaffe",
+  fields: [
+    {
+      name: "name",
+      title: "Navn på drikke",
+      type: "string",
+      validation: rule => rule.required().min(3).max(20)
+    },{
+      name: 'allergies',
+      title: 'Allergener',
+      type: 'array',
+      of: [{ type: 'string' }],
+      options: {
+        list: [
+          { title: 'Soya', value: 'soya' },
+          { title: 'Melk', value: 'milk' },
+          { title: 'Nøtter', value: 'nuts' },
+          { title: 'Gluten', value: 'gluten' },
+        ]
+      }
+    },{
+      name: "desc",
+      title: "Beskrivelse",
+      type: "string",
+    },{
+      name: 'image',
+      title: 'Bilde',
+      type: 'image',
+    }
+  ]
+}
