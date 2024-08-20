13ad6493a0c15bc6d9c05dd152c1e3d2886f59ff
kristoffer <kunkristoffer@gmail.com>
2024-08-20 11:01:17 +0200

    Adding placeholder for common events

---
+++ schemas-proposal/commonEvents.js
@@ -0,0 +1,14 @@
+export default {
+  title: 'Aktiviteter',
+  name: 'events',
+  type: 'document',
+  fields: [
+    {
+      title: 'Aktivitets navn',
+      name: 'name',
+      type: 'string',
+      description: 'Navn på denne aktiviteten, e.g; møte, samling eller fritidsaktivitet',
+      validation: rule => rule.required().min(10).max(30)
+    }
+  ]
+}
