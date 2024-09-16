4319b3dafeb7b6bccfc7c222e4063ff1ce8c905e
Helga-Vi <katarina.jorgensen@gmail.com>
2024-09-16 10:37:55 +0200

    Event schema til kafe

---
+++ schemas/kafeevent.js
@@ -0,0 +1,28 @@
+export default {
+    name: "kafe",
+    type: "document",
+    title: "Cafe",
+    fields: [
+
+        {
+    name: "title",
+    type: "string",
+    title: "Overskrift"
+        },
+          {
+            name: "beskrivelse",
+            type: "string",
+            title: "Beskrivelser",
+          },
+
+          {
+            name: 'image',
+            type: 'image',
+            title: 'Bilde',
+          },
+
+
+
+
+    ]
+}
