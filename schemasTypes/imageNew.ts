bf4380b7f47755a498ab76edc3d92a25f338028f
kristoffer <kunkristoffer@gmail.com>
2025-04-03 12:00:34 +0200

    converting schema files to use typescript

---
+++ schemasTypes/imageNew.ts
@@ -0,0 +1,59 @@
+export default {
+  name: "images",
+  type: "document",
+  title: "Bilde Galleri v2",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+      title: "Tema",
+    },
+    {
+      name: "imgOne",
+      type: "image",
+      title: "bilde",
+    },
+
+    {
+      name: "imgTwo",
+      type: "image",
+      title: "bilde",
+    },
+
+    {
+      name: "imgThree",
+      type: "image",
+      title: "bilde",
+    },
+
+    {
+      name: "imgFour",
+      type: "image",
+      title: "bilde",
+    },
+
+    {
+      name: "imgFive",
+      type: "image",
+      title: "bilde",
+    },
+
+    {
+      name: "imgSix",
+      type: "image",
+      title: "bilde",
+    },
+
+    {
+      name: "imgSeven",
+      type: "image",
+      title: "bilde",
+    },
+
+    {
+      name: "index",
+      type: "number",
+      title: "index",
+    },
+  ],
+};
