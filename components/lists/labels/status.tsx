b68969de38ea373445ff25c63a5b5f307a4579ea
kristoffer <kunkristoffer@gmail.com>
2025-04-05 16:14:07 +0200

    updated status label component to accept errors

--- components/lists/labels/status.tsx
+++ components/lists/labels/status.tsx
@@ -1,12 +1,35 @@
 import { Badge } from "@sanity/ui";
 import { TrelloApiItemStatus } from "../../../types/trello.types";
 
-export function StatusLabel({ status, hideNew = false, }: { status: TrelloApiItemStatus; hideNew?: boolean; }) {
+export function StatusLabel({
+  status,
+  hideNew = false,
+}: {
+  status: TrelloApiItemStatus;
+  hideNew?: boolean;
+}) {
   // Hide new tag if toggled
   if (hideNew && status === "new") return null;
 
-  const tone = status === "duplicate" ? "caution" : status === "processed" ? "positive" : "primary";
-  const message = status === "duplicate" ? "Duplikat" : status === "processed" ? "Behandlet" : "Ny";
+  let tone: "primary" | "positive" | "caution" | "critical";
+  let message;
 
-  return <Badge tone={tone}>{message}</Badge>;
+  switch (true) {
+    case status === "duplicate":
+      tone = "caution";
+      message = "Duplikat";
+      break;
+    case status === "processed":
+      tone = "positive";
+      message = "Behandlet";
+      break;
+    case status === "failed":
+      tone = "critical";
+      message = "Feil";
+    default:
+      tone = "primary";
+      message = "Ny";
+  }
+
+  return <Badge tone='positive'>{message}</Badge>;
 }
