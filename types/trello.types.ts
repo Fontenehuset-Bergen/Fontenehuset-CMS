806465660a56442d6acb5bcebc21e85a3514fefc
kristoffer <kunkristoffer@gmail.com>
2025-04-05 11:39:11 +0200

    improved trello response type

--- types/trello.types.ts
+++ types/trello.types.ts
@@ -1,13 +1,21 @@
 export interface TrelloApiItem {
-  id: string
-  name: string
-  desc: string
-  labels: {
-    name: string
-  }[]
-  cover: {}
+  id: string;
+  name: string;
+  desc: string;
+  due?: Date | null;
+  labels?: {
+    name: string;
+  }[];
+  cover?: {
+    scaled: {
+      id: string;
+      url: string;
+    }[];
+  };
+  attachments?: {
+    id: string;
+    url: string;
+  }[];
 }
 
-export interface TrelloApiResponse {
-
-}
+export interface TrelloApiResponse {}
