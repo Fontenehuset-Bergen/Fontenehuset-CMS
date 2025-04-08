687f4b09952704987d6b4ce1ea9f54603c0ebe22
Kristoffer <kunkristoffer@gmail.com>
2025-04-08 15:56:44 +0200

    updated menu line props and display

--- components/lists/logger/line.tsx
+++ components/lists/logger/line.tsx
@@ -3,27 +3,33 @@ import { MenuItemCard } from "../../cards/menuItem";
 import { TrelloApiItem } from "../../../types/trello.types";
 import { StatusLabel } from "../labels/status";
 
-export function LoggerLine(menuItem: TrelloApiItem) {
-  // todo add button for removing a single item
-  const time = new Date();
+// todo add button for removing a single item
+export function LoggerLine(lunchDish: TrelloApiItem) {
+  const { id, name, desc, start, cover, labels, status } = lunchDish;
+  const due = start ? new Date(start) : undefined;
+  const currentTime = new Date();
   return (
     <Card>
       <Flex gap={4} align={"center"}>
-        <Text muted>{time.toLocaleTimeString("no-NB")}</Text>
+        <Text muted>{currentTime.toLocaleTimeString("no-NB")}</Text>
         <Tooltip
-          content={<MenuItemCard {...menuItem} />}
+          content={<MenuItemCard {...lunchDish} />}
           animate
           fallbackPlacements={["right", "left"]}
           placement='top'
           portal
         >
-          <Text>{menuItem.name}</Text>
+          <Text>{name}</Text>
         </Tooltip>
         <Box flex={1}></Box>
-        {menuItem.status && <StatusLabel status={menuItem.status} hideNew/>}
-        <Text muted>
-          {time.toLocaleDateString("no-NB", { dateStyle: "long" })}
-        </Text>
+        {status && <StatusLabel status={status} />}
+        <Box style={{width: 100, textAlign: "end"}}>
+          <Text muted>
+            {due
+              ? due.toLocaleDateString("no-NB", { dateStyle: "long" })
+              : "dato mangler"}
+          </Text>
+        </Box>
       </Flex>
     </Card>
   );
