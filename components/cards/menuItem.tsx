f2020e3656badc8a771cf445d761f9d325f8ca7f
Kristoffer <kunkristoffer@gmail.com>
2025-04-08 15:32:25 +0200

    updated preview component, now feature complete

--- components/cards/menuItem.tsx
+++ components/cards/menuItem.tsx
@@ -1,19 +1,51 @@
-import { Badge, Box, Card, Flex, Inline, Stack, Text } from "@sanity/ui";
+import {
+  Box,
+  Card,
+  Flex,
+  Stack,
+  Text,
+} from "@sanity/ui";
 import { TrelloApiItem } from "../../types/trello.types";
 import { LabelsInline } from "../lists/labels/inline";
-import testTrelloApiItem from "../../tests/data/trelloApiItem.json";
+import { useEffect, useState } from "react";
 
-export function MenuItemCard({ name, desc, labels }: TrelloApiItem) {
-  const time = new Date();
+export function MenuItemCard({
+  name,
+  desc,
+  labels,
+  start,
+  cover,
+}: TrelloApiItem) {
+  const [time, setTime] = useState<Date | undefined>(undefined);
+  const [image, setImage] = useState<string | undefined>(undefined);
 
+  useEffect(() => {
+    if (start) {
+      setTime(new Date(start));
+    }
+    if (cover.scaled?.length) {
+      const firstImage = cover.scaled.at(1);
+      setImage(firstImage?.url);
+    }
+  }, []);
   return (
     <Card
       padding={2}
       style={{ minWidth: 200, maxWidth: "50svw", width: "100%" }}
     >
       <Stack space={4}>
-        <Flex gap={3}>
-          <Box>image</Box>
+        <Flex align={"center"} gap={3}>
+          <Card border style={{ width: 50, height: 50, overflow: "hidden" }}>
+            <img
+              src={image}
+              alt='Bilde'
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover"
+              }}
+            />
+          </Card>
           <Stack flex={1} space={3}>
             <Flex align={"center"}>
               <Box flex={1}>
@@ -22,13 +54,19 @@ export function MenuItemCard({ name, desc, labels }: TrelloApiItem) {
                 </Text>
               </Box>
               <Text size={3} muted>
-                {time.toLocaleDateString("no-NB", { dateStyle: "full" })}
+                {time
+                  ? time.toLocaleDateString("no-NB", { dateStyle: "full" })
+                  : "Ingen dato"}
               </Text>
             </Flex>
             {labels && <LabelsInline labels={labels} />}
           </Stack>
         </Flex>
-        {desc ? <Text>{desc}</Text> : <Text muted>Beskrivelse på matrett mangler</Text>}
+        {desc ? (
+          <Text>{desc}</Text>
+        ) : (
+          <Text muted style={{fontStyle: "italic"}}>Beskrivelse på matrett mangler</Text>
+        )}
       </Stack>
     </Card>
   );
