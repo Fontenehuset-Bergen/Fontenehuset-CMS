43863bd490b07cdb451ddd52f41ee1a698e08b64
Kristoffer <kunkristoffer@gmail.com>
2025-06-06 10:12:47 +0200

    Added better ordering and previewing to onboarding schema

--- schemasTypes/onboarding.ts
+++ schemasTypes/onboarding.ts
@@ -24,4 +24,30 @@ export default {
       title: "index",
     },
   ],
+  preview: {
+    select: {
+      title: "title",
+      index: "index",
+      media: "image",
+    },
+    prepare({ title, index, media }: Record<string, unknown>) {
+      return {
+        title,
+        subtitle: index ? `#${index}` : "har ingen index!",
+        media,
+      };
+    },
+  },
+  orderings: [
+    {
+      title: "Index, først til sist",
+      name: "IndexAsc",
+      by: [{ field: "index", direction: "asc" }],
+    },
+    {
+      title: "Index, sist til først",
+      name: "indexDesc",
+      by: [{ field: "index", direction: "desc" }],
+    },
+  ],
 };
